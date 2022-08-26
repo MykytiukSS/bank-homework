a7479e7c2c16b227c11df9b6a28242c726415d2b
MykytiukSS <m.sergeysergeyevich@gmail.com>
2022-08-26 14:17:50 +0300

    app.java was modified

--- src/main/java/com/example/bank/App.java
+++ src/main/java/com/example/bank/App.java
@@ -61,6 +61,142 @@ public class App
         }
     }
 
+    private static void transferMoney() {
+        Query query3;
+        Double amont;
+
+        System.out.println("Please, enter account Id:");
+        String searchYourAccountId = sc.nextLine();
+
+        Account accountFrom = em.find(Account.class, Long.parseLong(searchYourAccountId));
+        if (accountFrom == null) {
+            System.out.println("Account not found!");
+            return;
+        }
+
+        System.out.println("Enter receiver's account Id:");
+        String receiverAccountId = sc.nextLine();
+
+        Account receiverAccount = em.find(Account.class, Long.parseLong(receiverAccountId));
+        if (receiverAccount == null) {
+            System.out.println("Account not found !");
+            return;
+        }
+
+        System.out.println("Enter Amount to transfer:");
+        String sAmount = sc.nextLine();
+        int amount = Integer.parseInt(sAmount);
+        Double amountD = Double.valueOf(amount);
+        if (amount > accountFrom.getSum()) {
+            System.out.println("Not enough money!");
+            return;
+        }
+
+        em.getTransaction().begin();
+        try {
+            Transactions transaction = new Transactions(amountD, accountFrom, receiverAccount);
+            em.persist(transaction);
+
+            if (accountFrom.getValuta() != receiverAccount.getValuta()) {
+
+                if (accountFrom.getValuta() == "UAH") {
+                    amountD = amountD;
+                } else if (accountFrom.getValuta() == "USD") {
+
+
+                    amountD = amountD * 39;
+                } else {
+                    amountD = amountD * 41;
+                }
+            }
+            Double temp = Double.valueOf(accountFrom.getSum());
+            accountFrom.setSum(temp - amountD);
+
+            Double temp1 = Double.valueOf(receiverAccount.getSum());
+            receiverAccount.setSum(temp + amountD);
+
+            em.getTransaction().commit();
+            System.out.println("Money Transferred!");
+
+
+        }    catch (Exception ex) {
+            ex.printStackTrace();
+            em.getTransaction().rollback();
+            return;
+        }
+    }
+
+    private static void viewUsers() {
+        try {
+            Query query1 = em.createQuery("SELECT u FROM Users u ORDER BY u.name", Users.class);
+            List<Users> userList = query1.getResultList();
+
+            for (Users user : userList) {
+                System.out.println(user);
+            }
+        } catch (NoResultException ex) {
+            System.out.println("There are no users!");
+            return;
+        }
+    }
+
+    private static void addMoneyToAccount() {
+        Double ammount = 0.0;
+        Query query2;
+
+        System.out.println("Please, enter account Id:");
+        String searchAccountId = sc.nextLine();
+
+        Account account = em.find(Account.class, Long.parseLong(searchAccountId));
+        if (account == null) {
+            System.out.println("Account not found!");
+            return;
+        }
+
+        System.out.print("Select currency: 1 - UAH, 2 - USD, 3 - EUR");
+        String sCurrency = sc.nextLine();
+        Curses currency = new Curses();
+        currency.setName("UAH");
+        if (sCurrency.equals("2"))
+            currency.setName("USD");
+        else if (sCurrency.equals("3"))
+            currency.setName("EUR");
+
+
+        System.out.println("Enter Amount:");
+        String sAmount = sc.nextLine();
+        int amount = Integer.parseInt(sAmount);
+        Double amnt = ammount;
+
+        em.getTransaction().begin();
+        try {
+            if (!currency.getName().equals(account.getValuta())) {
+
+
+                if (currency.getName() == "UAH") {
+                    ammount = amnt;
+                } else if (currency.getName() == "USD") {
+
+                    ammount = amnt * 39;
+                } else {
+                    ammount = amnt * 41;
+                }
+
+                Transactions transaction = new Transactions(ammount, account) {
+
+                };
+                em.persist(transaction);
+                em.getTransaction().commit();
+                System.out.println("Money Added!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+            return;
+        }
+    }
+
+
     private static void rates_init() {
         em.getTransaction().begin();
         Curses kursUsd = new Curses("USD", 39.1);
@@ -111,142 +247,12 @@ public class App
             return;
         }
 
-        private static void viewUsers() {
-            try {
-                Query query1 = em.createQuery("SELECT u FROM Users u ORDER BY u.name", Users.class);
-                List<Users> userList = query.getResultList();
-
-                for (Users user : userList) {
-                    System.out.println(user);
-                }
-            } catch (NoResultException ex) {
-                System.out.println("There are no users!");
-                return;
-            }
-        }
-
-        private static void addMoneyToAccount() {
-            Double ammount = 0.0;
-            Query query2;
-
-            System.out.println("Please, enter account Id:");
-            String searchAccountId = sc.nextLine();
-
-            Account account = em.find(Account.class, Long.parseLong(searchAccountId));
-            if (account == null) {
-                System.out.println("Account not found!");
-                return;
-            }
-
-            System.out.print("Select currency: 1 - UAH, 2 - USD, 3 - EUR");
-            String sCurrency = sc.nextLine();
-            Curses currency = new Curses();
-            currency.setName("UAH");
-            if (sCurrency.equals("2"))
-                currency.setName("USD");
-            else if (sCurrency.equals("3"))
-                currency.setName("EUR");
-
-
-            System.out.println("Enter Amount:");
-            String sAmount = sc.nextLine();
-            int amount = Integer.parseInt(sAmount);
-            Double amnt = ammount;
-
-            em.getTransaction().begin();
-            try {
-                if (!currency.getName().equals(account.getValuta())) {
-
-
-                    if (currency.getName() == "UAH") {
-                        ammount = amnt;
-                    } else if (currency.getName() == "USD") {
-
-                        ammount = amnt * 39;
-                    } else {
-                        ammount = amnt * 41;
-                    }
-
-                    Transactions transaction = new Transactions(ammount, account) {
-
-                    };
-                    em.persist(transaction);
-                    em.getTransaction().commit();
-                    System.out.println("Money Added!");
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                em.getTransaction().rollback();
-                return;
-            }
-        }
-
-        private static void transferMoney () {
-            Query query3;
-            Double amont;
-
-            System.out.println("Please, enter account Id:");
-            String searchYourAccountId = sc.nextLine();
-
-            Account accountFrom = em.find(Account.class, Long.parseLong(searchYourAccountId));
-            if (accountFrom == null) {
-                System.out.println("Account not found!");
-                return;
-            }
-
-            System.out.println("Enter receiver's account Id:");
-            String receiverAccountId = sc.nextLine();
-
-            Account receiverAccount = em.find(Account.class, Long.parseLong(receiverAccountId));
-            if (receiverAccount == null) {
-                System.out.println("Account not found !");
-                return;
-            }
-
-            System.out.println("Enter Amount to transfer:");
-            String sAmount = sc.nextLine();
-            int amount = Integer.parseInt(sAmount);
-            Double amountD = Double.valueOf(amount);
-            if (amount > accountFrom.getSum()) {
-                System.out.println("Not enough money!");
-                return;
-            }
-
-            em.getTransaction().begin();
-            try {
-                Transactions transaction = new Transactions(amountD, accountFrom, receiverAccount);
-                em.persist(transaction);
-
-                if (accountFrom.getValuta() != receiverAccount.getValuta()) {
-
-                    if (accountFrom.getValuta() == "UAH") {
-                        amountD = amountD;
-                    } else if (accountFrom.getValuta() == "USD") {
-
-
-                        amountD = amountD * 39;
-                    } else {
-                        amountD = amountD * 41;
-                    }
-                }
-                Double temp = Double.valueOf(accountFrom.getSum());
-                accountFrom.setSum(temp - amountD);
-
-                Double temp1 = Double.valueOf(receiverAccount.getSum());
-                receiverAccount.setSum(temp + amountD);
-
-            em.getTransaction().commit();
-            System.out.println("Money Transferred!");
-
-
-    }    catch (Exception ex) {
-            ex.printStackTrace();
-            em.getTransaction().rollback();
-            return;
-        }
-    }
 
 
 
 
+
+
+
+}
 }
